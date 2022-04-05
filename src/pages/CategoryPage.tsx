71f2862af6ee6748a7dc06b54a3f6a4335f42f39
Rinor Rama <rinor.rama95@gmail.com>
2022-04-05 11:01:51 +0200

    can view articles in the different pages

--- src/pages/CategoryPage.tsx
+++ src/pages/CategoryPage.tsx
@@ -1,5 +1,20 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import ArticleList from '../components/ArticleList/ArticleList';
+
 function CategoryPage() {
-  return <div>CategoryPage</div>;
+  const [articles, setArticles] = useState<Article[]>([]);
+  const params = useParams();
+  useEffect(() => {
+    fetch(`http://localhost:4000/articles/${params.category}`)
+      .then((resp) => resp.json())
+      .then((articles) => setArticles(articles));
+  }, [params]);
+  return (
+    <div className='main'>
+      <ArticleList articles={articles} setArticles={setArticles} />
+    </div>
+  );
 }
 
 export default CategoryPage;
--- src/pages/UserArticles.tsx
+++ src/pages/UserArticles.tsx
@@ -1,5 +1,20 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
+import ArticleList from '../components/ArticleList/ArticleList';
+
 function UserArticles() {
-  return <div>UserArticles</div>;
+  const [articles, setArticles] = useState<Article[]>([]);
+  const params = useParams();
+  useEffect(() => {
+    fetch(`http://localhost:4000/users/${params.username}`)
+      .then((resp) => resp.json())
+      .then((data) => setArticles(data.user.articles));
+  }, [params]);
+  return (
+    <div className='main'>
+      <ArticleList articles={articles} setArticles={setArticles} />
+    </div>
+  );
 }
 
 export default UserArticles;
