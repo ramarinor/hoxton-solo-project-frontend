84c892934074d37cad44b9bac87e5729cd30a7d9
Rinor Rama <rinor.rama95@gmail.com>
2022-04-06 15:49:37 +0200

    added admin page

---
+++ src/components/User/User.tsx
@@ -0,0 +1,53 @@
+import { MenuItem, Select } from '@mui/material';
+import { useState } from 'react';
+import { useStore } from '../../store';
+import './User.css';
+type Props = {
+  user: User;
+};
+function User({ user }: Props) {
+  const setModalMessage = useStore((store) => store.setModalMessage);
+  const [roleId, setRoleId] = useState<number>(user.roleId);
+  function editUserRole(roleId: number) {
+    fetch(`http://localhost:4000/users/${user.id}`, {
+      method: 'PATCH',
+      headers: {
+        Authorization: localStorage.token,
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ roleId })
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        if (data.error) setModalMessage(data.error);
+        else setModalMessage(data.message);
+        setRoleId(data.user.roleId);
+      });
+  }
+
+  return (
+    <div className='user' key={user.id}>
+      <img src={`${user.image}`} />
+      <b>{user.username}</b>
+      <div>
+        <Select
+          fullWidth
+          name='role'
+          value={roleId}
+          displayEmpty
+          inputProps={{ 'aria-label': 'Without label' }}
+          required
+          onChange={(e) => {
+            editUserRole(Number(e.target.value));
+          }}
+        >
+          <MenuItem value={1}>Admin</MenuItem>
+          <MenuItem value={2}>Journalist</MenuItem>
+          <MenuItem value={3}>User</MenuItem>
+        </Select>
+      </div>
+    </div>
+  );
+}
+
+export default User;
--- src/pages/UsersAdminPage.tsx
+++ src/pages/UsersAdminPage.tsx
@@ -1,5 +1,34 @@
+import { useEffect, useState } from 'react';
+import { useStore } from '../store';
+import User from '../components/User/User';
+
 function UsersAdminPage() {
-  return <div className='main'>UsersAdminPage</div>;
+  const [users, setUsers] = useState<User[]>([]);
+  const setModalMessage = useStore((store) => store.setModalMessage);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/users', {
+      headers: {
+        Authorization: localStorage.token
+      }
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        if (data.error) {
+          setModalMessage(data.error);
+        } else {
+          setUsers(data.users);
+        }
+      });
+  }, []);
+
+  return (
+    <div className='users'>
+      {users.map((user) => (
+        <User user={user} />
+      ))}
+    </div>
+  );
 }
 
 export default UsersAdminPage;
